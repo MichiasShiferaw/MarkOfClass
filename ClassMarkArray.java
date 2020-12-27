955dacc87885eeccf3199ec3f01d3502d08d8c6a
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2020-12-27 13:26:49 -0500

    Update ClassMarkArray.java

--- ClassMarkArray.java
+++ ClassMarkArray.java
@@ -1,3 +1,10 @@
 public class ClassMarksArray {
 
   public static int classAverage(int[][] marksArray) {
+    
+    //Declare variables
+    int total = 0;
+    //int totalNum = 0;
+    int counter = 0;
+
+    for (int x = 0; x < marksArray.length; x++) {
